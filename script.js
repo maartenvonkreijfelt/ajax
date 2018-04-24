bfbe371cf6516f938341c5bedd7ed8aa841ba63a
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-04-24 11:22:47 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-for (var i = 0; i < 100; i++) {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'data.txt', false);
-    request.send();
-    if (request.status===200) {
+var request;
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+request.open('GET', 'data.txt');
+request.onreadystatechange = function() {
+    if ((request.readyState===4) && (request.status===200)) {
         console.log(request);
         document.writeln(request.responseText);
     }
 }
+request.send();
+
