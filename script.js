9a04ed0ab038eff3ef221001a85a9e0f5cf3557c
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-04-25 12:09:45 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+function loadAJAX() {
 var request;
 if (window.XMLHttpRequest) {
 	request = new XMLHttpRequest();
 } else {
 	request = new ActiveXObject("Microsoft.XMLHTTP");
 }
+
 request.open('GET', 'data.json');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200)) {
@@ -17,3 +19,4 @@ request.onreadystatechange = function() {
 	}
 }
 request.send();
+} // loadAJAX
