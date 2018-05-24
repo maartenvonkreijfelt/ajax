0161b438d57a790e0be83a48e602aafdd112f6bf
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2018-05-24 09:31:18 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-$.getJSON('data.json', function(data) {
-	var output = '<ul>';
-	$.each(data, function(key, val) {
-		output += '<li>' + val.name + '</li>';
-	});
-	output +='</ul>';
-	$('#update').prepend(output);
-});
